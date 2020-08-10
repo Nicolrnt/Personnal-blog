c710bc1c65a7f2438abda637535b0bfe71463549
Nicolas <nicolas-laurent@outlook.fr>
2020-08-10 22:49:06 +0200

    feat: base is done, responsive left

---
+++ home/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hero from './Hero';
+import Work from './Work';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Work />
+    </div>
+  );
+}
+
+export default App;
