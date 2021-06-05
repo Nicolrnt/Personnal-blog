d2a769ae7ed0958c43ccbe9512b7d90e83a646f2
Nicolas <nicolas-laurent@outlook.fr>
2021-06-05 19:29:26 +0200

    feat: more minimalist

--- home/src/Home/index.js
+++ home/src/Home/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Container, Emoji, Text } from './Home.module.css';
+import { Container, Emoji } from './Home.module.css';
 
 const Home = () => {
   return (
     <div className={Container}>
       <div className={Emoji}>
         <img src='/dev.png' alt='dev' />
-        <h1 className={Text}>Nicolas Laurent</h1>
       </div>
     </div>
   );
